eae15ce457f88fef82f85a1df99ccb1381f03652
uncertify <uncertifieddevice@gmail.com>
2019-12-08 16:33:27 +0000

    Basic receiving prototype on i2c address 1

--- key-receiver.ino
+++ key-receiver.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+void setup()
+{
+  Wire.begin(1);
+  Wire.onReceive(receiveCharacter);
+
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  delay(50);
+}
 
+void receiveCharacter(int n)
+{
+  while(1 < Wire.available())
+  {
+    Wire.read();
+  }
+
+  //display(Wire.read());
+  Serial.println(Wire.read());
 }
